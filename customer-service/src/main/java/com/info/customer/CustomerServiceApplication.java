4a4693e82382677079947c73f868fcc237316ce9
AmitDighe <AmitDighe@TDLT-064>
2026-01-20 20:45:22 +0530

    added comments

--- customer-service/src/main/java/com/info/customer/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/info/customer/CustomerServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CustomerServiceApplication {
 
+	//start spring boot application here
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerServiceApplication.class, args);
 	}
